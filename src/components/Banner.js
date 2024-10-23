c092f42663cd5e977a411848dfaee805de838f81
nathsoto1 <nsoto1@lsu.edu>
2024-10-23 14:35:00 -0500

    add background

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -46,7 +46,7 @@ export  const Banner = () => {
     <Container>
       <Row className="align-items-center">
       <Col xs={12} md={6} xl={7}>
-        <span className="tagline">Welcome to my Portfolio</span>
+        <span className="tagline">Welcome to my Portfolio!</span>
         <h1>{`Hi I'm `}<span className="wrap"> {text}</span></h1>
         <div className="box">
         <p className="inside-box">Currently, I am on the lookout for software engineering internships.</p>
